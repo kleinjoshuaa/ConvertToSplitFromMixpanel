fe58192e5a369a2a2f9c57793b8a6d12c97e9803
Joshua Klein <klein.joshua.a@gmail.com>
2022-04-19 09:01:44 -0400

    updated

--- Readme.md
+++ Readme.md
@@ -2,11 +2,18 @@
 
 The provided code is a lambda function that can be uploaded and used as a way to send mixpanel events to split
 
-It reads 2 environment variables. process.env.ENVIRONMENT for the name of the split environment, and process.env.API_KEY for the split api key. 
+It reads 5 lambda environment variables. 
+ - ENVIRONMENT for the name of the split environment
+ - API_KEY for the split api key
+ - BATCH_SIZE for the batch size to send events to Split
+ - RETRY_COUNT for the number of retry attempts upon api call failure 
+ - SLEEP_MS for the sleep time between retrys and batch sizes in milliseconds
 
-This uses the [Create Event](https://docs.split.io/reference/create-event) Split REST API. 
+This uses the [Create Events](https://docs.split.io/reference/create-events) Split REST API. 
 
 
 It needs to be run with a lambda function that has access rights to read the S3 bucket used as a raw data pipeline, as set up following this [Mixpanel documentation](https://developer.mixpanel.com/docs/aws-raw-pipeline)
 
 Then you will need to set up the S3 bucket to create an event notification that triggers this Lambda function. This can be configured in the bucket properties.
+
+The Lambda function will determine which bucket and key to process from the S3 event supplied to it. 
--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 const { ungzip } = require("node-gzip");
-var https = require("follow-redirects").https;
-var fs = require("fs");
+const https = require("follow-redirects").https;
 
-// set API key variables
+const batchSize = process.env.BATCH_SIZE;
+const numRetries = process.env.NUM_RETRIES;
+const environment = process.env.ENVIRONMENT;
+const sleepMs = process.env.SLEEP_MS;
+const apiKey = process.env.API_KEY;
+
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+let sampleEvent = {  
+  "Records":[  
+     {  
+        "eventVersion":"2.2",
+        "eventSource":"aws:s3",
+        "awsRegion":"us-east-2",
+        "eventTime":"1970-01-01T00:00:00.000Z",
+        "eventName":"event-type",
+        "userIdentity":{  
+           "principalId":"joshKlein"
+        },
+        "requestParameters":{  
+           "sourceIPAddress":"192.168.1.1"
+        },
+        "responseElements":{  
+           "x-amz-request-id":"Amazon S3 generated request ID",
+           "x-amz-id-2":"Amazon S3 host that processed the request"
+        },
+        "s3":{  
+           "s3SchemaVersion":"1.0",
+           "configurationId":"ID found in the bucket notification configuration",
+           "bucket":{  
+              "name":"mixpanel-to-split",
+              "ownerIdentity":{  
+                 "principalId":"Amazon-customer-ID-of-the-bucket-owner"
+              },
+              "arn":"arn:aws:s3:::mixpanel-to-split"
+           },
+           "object":{  
+              "key":"2695663/2022/04/18/full_day/complete",
+              "size":"object-size in bytes",
+              "eTag":"object eTag",
+              "versionId":"object version if bucket is versioning-enabled, otherwise null",
+              "sequencer": "a string representation of a hexadecimal value used to determine event sequence, only used with PUTs and DELETEs"
+           }
+        },
+        "glacierEventData": {
+           "restoreEventData": {
+              "lifecycleRestorationExpiryTime": "The time, in ISO-8601 format, for example, 1970-01-01T00:00:00.000Z, of Restore Expiry",
+              "lifecycleRestoreStorageClass": "Source storage class for restore"
+           }
+        }
+     }
+  ]
+}
+
+
 
 function createRequestOptions() {
-  var options = {
+  let options = {
     method: "POST",
     hostname: "events.split.io",
     path: "/api/events/bulk",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + process.env.API_KEY,
+      Authorization: "Bearer " + apiKey,
     },
     maxRedirects: 20,
   };
@@ -25,7 +81,7 @@ function createRequestOptions() {
 function formatMixPanelEvent(mixPanelEvent) {
   let result = {
     eventTypeId: mixPanelEvent.event,
-    environmentName: process.env.ENVIRONMENT,
+    environmentName: environment,
     trafficTypeName: "user",
     key: mixPanelEvent.properties.distinct_id,
     timestamp: mixPanelEvent.properties.time * 1000,
@@ -36,41 +92,68 @@ function formatMixPanelEvent(mixPanelEvent) {
   return result;
 }
 
+
+
+async function sendHTTPEvents(options, events, retryCount){
+  let req = https.request(options, function (res) {
+    let chunks = [];
+    if(res.statusCode != 202) {
+      console.error("Status Code: "+res.statusCode+" the events were not imported");
+      if(retryCount == numRetries){
+        console.error('try '+retryCount+' failed, will not retry')
+      } else {
+        retryCount++
+        sleep(sleepMs)
+        console.error('Retry number '+retryCount)
+        sendHTTPEvents(options, events, retryCount)
+      }
+    } else {
+      console.log('success')
+    }
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+  
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+    });
+    
+    res.on("error", function (error) {
+      console.error(error);
+      if(retryCount == numRetries){
+        console.error(retryCount+' try failed, will not retry')
+      } else {
+        retryCount++
+        sleep(sleepMs)
+        console.error('Retry number '+retryCount)
+        sendHTTPEvents(options, events, retryCount)
+      }
+    });
+  });
+  req.write(JSON.stringify(events));
+  req.end();
+
+}
+
 exports.handler = async (event, context) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = decodeURIComponent(
-    event.Records[0].s3.object.key
-      .replace(/\+/g, " ")
-      .replace("complete", "export.json.gz")
+   event.Records[0].s3.object.key
+    .replace(/\+/g, " ")
+     .replace("complete", "export.json.gz")
   );
   const data = await s3.getObject({ Bucket, Key }).promise();
+
   const json = (await ungzip(data.Body)).toString().split("\n").slice(0, -1);
-//   let body = formatMixPanelEvent(JSON.parse(v));
-  let options = createRequestOptions(body);
-  var req = https.request(options, function (res) {
-    var chunks = [];
-    let data = [];
-    json.forEach(function (v, i, a) {
-        // add each event
-        data.push(formatMixPanelEvent(JSON.parse(v)))    
-    });
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
+  let options = createRequestOptions();
 
-      res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
-
-      res.on("error", function (error) {
-        console.error(error);
-      });
-    });
-
-    //console.log(JSON.stringify(body));
-    req.write(JSON.stringify(body));
-
-    req.end();
+  for(let jsonIdx = 0; jsonIdx < json.length; jsonIdx += batchSize) {
+    let batchData = [];
+    for(let batchIdx = jsonIdx; batchIdx < Math.min(jsonIdx+batchSize, json.length); batchIdx+=1) {
+      batchData.push(formatMixPanelEvent(JSON.parse(json[batchIdx])));
+    }
+    await sendHTTPEvents(options, batchData, 0);
+    sleep(sleepMs)
+  }
 
 };
